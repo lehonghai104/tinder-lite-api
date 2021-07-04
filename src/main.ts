b9459297c62387f3e47299e8f3f111cb9667bbba
haile <lehonghai104@gmail.com>
2021-07-04 23:50:15 +0700

    add basic swagger api-docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Tinder Lite APIs')
+    .setDescription('Basic api for Tinder Lite web-app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
